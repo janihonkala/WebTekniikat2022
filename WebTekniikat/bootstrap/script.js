f680a843774a1bd9d2111e5f9b447eff7298044b
Jani Honkala <101732221+janihonkala@users.noreply.github.com>
2022-04-12 16:53:17 +0300

    Add files via upload

---
+++ WebTekniikat/bootstrap/script.js
@@ -0,0 +1,10 @@
+// Tapahtumankäsittelijä painikkeelle
+document.getElementById("submit").onclick = function () {
+    let name = document.getElementById("fname").value;
+    if (name.length > 0) {
+
+
+    } else {
+        
+    }
+}
