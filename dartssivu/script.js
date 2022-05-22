b7ccc2ab137543819c93729ac7ec6da630613847
Jani Honkala <101732221+janihonkala@users.noreply.github.com>
2022-05-22 14:33:56 +0300

    Add files via upload
    Lisätty darts aiheinen sivu palautustehtäväksi

---
+++ dartssivu/script.js
@@ -0,0 +1,7 @@
+document.body.style.background = 'lightgreen';
+setTimeout(() => document.body.style.background = '', 2500);
+
+let element = document.getElementById('content');
+element.style.background = 'red';
+
+alert(('Tervetuloa!') );
